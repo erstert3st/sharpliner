309a3524a7d60e283cabedc621ddf8d993e11143
erstert3st <lura12111@gmail.com>
2024-05-18 00:41:03 +0200

    remove create Folder + make script path  safe for paths with " "

--- eng/scripts/build-dependencies.ps1
+++ eng/scripts/build-dependencies.ps1
@@ -1,15 +1,17 @@
-$repo_root = Join-Path $PSScriptRoot ".."
-$repo_root = Join-Path $repo_root ".."
+param(
+    [string]$dotnetPack = "true"
+)
 
-if (-not(Test-Path "$repo_root/artifacts")) {
-    New-Item -Path "$repo_root" -Name "artifacts" -ItemType "directory" | out-null
-}
+$current_dir = $PSScriptRoot
+#get Grandparent directory
+$repo_root = Split-Path -Parent (Split-Path -Parent $current_dir)
+Write-Host "Repo root: $repo_root"
 
 if (-not(Test-Path "$repo_root/artifacts/packages")) {
-    New-Item -Path "$repo_root/artifacts" -Name "packages" -ItemType "directory" | out-null
+    New-Item -Path "$repo_root/artifacts/packages" -ItemType Directory -Force | Out-Null
 }
 
-if (-not(Test-Path "$repo_root/artifacts/packages/Sharpliner.43.43.43.nupkg")) {
+if (-not(Test-Path "$repo_root/artifacts/packages/Sharpliner.43.43.43.nupkg") -and $dotnetPack -ne "false") {
     Write-Host "Building Sharpliner nupkg for Sharpliner.CI..."
     dotnet pack --nologo "$repo_root/src/Sharpliner/Sharpliner.csproj" -p:PackageVersion=43.43.43 -c:Release
 }
